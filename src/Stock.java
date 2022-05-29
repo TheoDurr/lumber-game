164dfc58f1d8fbeaa8fa98631479099629fc3676
AlexisBouligand <alexisbouligand@gmail.com>
2022-05-29 15:03:24 +0200

    Implementatin of Stock.java

---
+++ src/Stock.java
@@ -0,0 +1,13 @@
+public class Stock {
+
+    private int maxCapacity;
+    private int currentCapacity;
+
+    public void addWood(int qtt){
+        currentCapacity+=qtt;
+    }
+
+    public void removeWood(int qtt){
+        currentCapacity+=qtt;
+    }
+}
