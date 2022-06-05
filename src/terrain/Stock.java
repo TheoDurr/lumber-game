0627b04696bb8d1d2024f1a80542ad6b3ce35f8a
AlexisBouligand <alexisbouligand@gmail.com>
2022-06-05 10:17:21 +0200

    Added commentary about how to handle Stock methods

--- src/terrain/Stock.java
+++ src/terrain/Stock.java
@@ -1,9 +1,14 @@
 package terrain;
 
+import wood.Wood;
+
+import java.util.List;
+
 public class Stock {
 
     private int maxCapacity = 20; //Need to be changed
     private int currentCapacity = 0;
+    private List<Wood> woodCapacity;
 
     public int getCurrentCapacity(){
         return currentCapacity;
@@ -20,12 +25,14 @@ public class Stock {
         return false;
     }
 
+    //Get a list of wood in argument
+    //Add this wood to woodCapacity
     public void addWood(int quantity){
         currentCapacity+=quantity;
-
-
     }
 
+    //Return a list of wood
+    //Remove this wood from the wood list of the stock
     public void removeWood(int quantity){
         currentCapacity-=quantity;
     }
