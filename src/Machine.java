5bc39457a812dbf3540dfee3664ee720dbadbec7
Théo DURR <git@theodurr.fr>
2022-06-07 15:45:54 +0200

    Added stocks and machines - WIP

--- src/Machine.java
+++ src/Machine.java
@@ -2,13 +2,35 @@ import java.util.List;
 
 public class Machine extends Factory {
 
-  public String name;
+    private String name;
 
-  public float price;
+    private float price;
 
-  public List<Factory> factory;
+    public Machine(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
 
-  public void transformWood() {
-  }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void transformWood(Tree t) {
+        if (t.getState() == TreeState.MATURE) {
+            t.nextState();
+        }
+    }
 
 }
