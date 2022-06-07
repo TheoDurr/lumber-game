5f54ad4963b79600ced3870b16d268dfd276149d
Théo DURR <git@theodurr.fr>
2022-06-07 19:40:36 +0200

    Added machine stocks

--- src/Machine.java
+++ src/Machine.java
@@ -6,9 +6,30 @@ public class Machine extends Factory {
 
     private float price;
 
+    private Stock inputStock;
+    private Stock outputStock;
+
     public Machine(String name, float price) {
         this.name = name;
         this.price = price;
+        this.inputStock = new Stock();
+        this.outputStock = new Stock();
+    }
+
+    public Stock getInputStock() {
+        return inputStock;
+    }
+
+    public void setInputStock(Stock inputStock) {
+        this.inputStock = inputStock;
+    }
+
+    public Stock getOutputStock() {
+        return outputStock;
+    }
+
+    public void setOutputStock(Stock outputStock) {
+        this.outputStock = outputStock;
     }
 
     public String getName() {
